518763434fd728095d85c6966a619f307a0d2dc1
Hakan Bilgi <hknblg34@gmail.com>
2022-10-16 13:14:25 +0300

    sekond

---
+++ src/Helper/data.js
@@ -0,0 +1,18 @@
+const data = [
+  {
+    id: 1,
+    name: "Front-end Developer",
+    image:
+      "https://clarusway.com/wp-content/uploads/elementor/thumbs/v1-Frontend-developer-prp8n0p67mzqvh8m3e9pqfj9umspqxy9bzk2kwssw8.jpg",
+    desc: "Become a master of HTML, CSS, and JavaScript for Front-End Development.",
+  },
+  {
+    id: 2,
+    name: "Back-end Developer",
+    image:
+      "https://clarusway.com/wp-content/uploads/elementor/thumbs/v1-Software-Developer-prp8n1n0eh117378xwocaxaqg0o2yn1zo47k26req0.jpg",
+    desc: "Focus on databases, programming, and website architecture on the server-side.",
+  },
+];
+
+export default data;
