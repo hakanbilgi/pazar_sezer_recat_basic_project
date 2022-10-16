3b7c92b7df67dc8152404172e5b3d6073ee4f288
Hakan Bilgi <hknblg34@gmail.com>
2022-10-16 13:23:52 +0300

    file start

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from '../Card/card'
+import "./Main.css"
+
+const Main = () => {
+  return (
+    <div>
+    <Card/>
+    </div>
+  )
+}
+
+export default Main
